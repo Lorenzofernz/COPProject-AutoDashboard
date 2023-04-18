8bd28c508470f806b8e0b64dbff6e1506159a83e
Lorenzo Fernandez <105619839+Lorenzofernz@users.noreply.github.com>
2023-04-17 22:15:41 -0400

    Update TemperatureGauge.java

--- COPFinalProject/TemperatureGauge.java
+++ COPFinalProject/TemperatureGauge.java
@@ -1,10 +1,15 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class TemperatureGauge {
     
-     int temp;
-     int maxTemp = 250;
-     int minTemp = 0;
-     int tempRange = maxTemp - minTemp;
-     boolean overheated = false;
+    private int temp =0;
+    private int maxTemp = 250;
+    private int minTemp = 0;
+    private int tempRange = maxTemp - minTemp;
+    //private boolean overheated = false;
+
+   // TemperatureGauge gauge = new TemperatureGauge();
 
     public  int getTemp(int temp) {
         temp = (int) (Math.random() * tempRange + minTemp);
@@ -12,11 +17,11 @@ public class TemperatureGauge {
     }
 
     public  void displayTemp() {
-        System.out.println("Current temperature: " + getTemp(temp));
+        currentTemp(getTemp(temp));
     }
 
     public  boolean isOverheated(int temp) {
-        if (temp > 220) {
+        if (temp >= 220) {
             return true;
         } else {
             return false;
@@ -27,4 +32,24 @@ public class TemperatureGauge {
         System.out.println("Vehicle is overheated: " + isOverheated(temp));
     }
     
+    public int currentTemp(int getTemp){
+        Queue<Integer> tempQueue = new LinkedList<Integer>();
+        int[] tempReadings = new int[10];
+        temp = 0;
+        for(int i = 0; i < tempReadings.length; i++){
+            tempReadings[i] = getTemp;
+        }
+        for(int temperature: tempReadings){
+            tempQueue.add(temperature);
+            temp = temperature;
+        } 
+        if(temp >= 220){
+            displayOverheated();
+        }
+        else if(temp < 220)
+         System.out.println("Temperature: " + temp + "°F");
+        return temp;
+        
+    }
+
 }
